136a6e5fb13d0fa3e393356a43061bbf06cabaae
Xen <aron.laika@gmail.com>
2026-02-09 19:35:59 +0100

    cambio para no tener que hacer todos los findViewById()

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/prueba1/MainActivity.kt
+++ app/src/main/java/com/example/prueba1/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.example.prueba1
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.prueba1.databinding.ActivityMainBinding
 
-public const val TAG1 = "Actividad 1"
+const val TAG1 = "Actividad 1"
+const val ETIQUETA = "Etiqueta"
 class MainActivity : AppCompatActivity() {
 
     val boton: Button by lazy {
         findViewById(R.id.button)
     }
+    val etiqueta: TextView by lazy {
+        findViewById(R.id.textView)
+    }
+    val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)        // para no tener que hacer todos los findViewById()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -32,9 +43,13 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d(TAG1, "Bundle no es nulo")
         }
+
+        binding.button.setOnClickListener {  }
+
         boton.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+            etiqueta.text = "Etiqueta cambiada"
+            //val intent = Intent(this, MainActivity2::class.java)
+            //startActivity(intent)
         }
         // Llama a override fun onClick(v: View?)
         // boton.setOnClickListener(this)
@@ -75,6 +90,18 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d(TAG1, "Bundle no es nulo")
         }
+        outState.putString(ETIQUETA, etiqueta.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG1, "Estoy en onRestoreInstanceState")
+        if (savedInstanceState == null) {
+            Log.d(TAG1, "Bundle es nulo")
+        } else {
+            Log.d(TAG1, "Bundle es no nulo")
+        }
+        etiqueta.text = savedInstanceState.getString(ETIQUETA)
     }
 
     override fun onRestart() {
